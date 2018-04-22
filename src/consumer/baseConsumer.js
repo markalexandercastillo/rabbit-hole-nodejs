52145848b2e1c8230c46dd03ecda4cef82810c22
Mark Castillo <markalexandercastillo@gmail.com>
2018-04-21 22:56:59 -0400

    Adding base consumer module within consumer module

---
+++ src/consumer/baseConsumer.js
@@ -0,0 +1,36 @@
+const create =
+  (channel, queue) =>
+    ({
+      /**
+       * See http://www.squaremobius.net/amqp.node/channel_api.html#channel_consume
+       * @callback cb
+       * @param {object} options
+       * @param {boolean} options.noLocal
+       * @param {string} options.consumerTag
+       * @param {boolean} options.noAck
+       * @param {boolean} options.exclusive
+       * @param {number} options.priority
+       * @param {object} options.arguments
+       */
+      consume: (cb, options = {}) =>
+        channel.consume(queue, cb, options),
+      /**
+       * See http://www.squaremobius.net/amqp.node/channel_api.html#channel_ack
+       * @param {object} message
+       * @param {boolean} [allUpTo=false]
+       */
+      ack: (message, allUpTo = false) =>
+        channel.ack(message, allUpTo),
+      /**
+       * See http://www.squaremobius.net/amqp.node/channel_api.html#channel_nack
+       * @param {object} message
+       * @param {boolean} [allUpTo=false]
+       * @param {boolean} [requeue=true]
+       */
+      nack: (message, allUpTo = false, requeue = true) =>
+        channel.nack(message, allUpTo, requeue),
+    });
+
+module.exports = {
+  create,
+};
---
+++ src/consumer/baseConsumer.spec.js
@@ -0,0 +1,109 @@
+const td = require('testdouble')
+  , BaseConsumer = require('./baseConsumer');
+
+const whenLoose = testDouble => td.when(testDouble, { ignoreExtraArgs: true });
+
+describe('BaseConsumer', () => {
+  context('Instance methods', () => {
+    let queue, channel, consumer;
+    beforeEach(() => {
+      queue = 'some-queue';
+      channel = td.object(['consume', 'prefetch', 'ack', 'nack']);
+      consumer = BaseConsumer.create(channel, queue);
+    });
+
+    describe('.ack', () => {
+      it('acks the given message', done => {
+        const expectedMessage = { someMessageKey: 'some-message-value' };
+        whenLoose(channel.ack(expectedMessage)).thenResolve();
+        consumer.ack(expectedMessage)
+          .then(() => done());
+      });
+
+      it('defaults allUpTo to false', done => {
+        td.when(channel.ack(td.matchers.anything(), false)).thenResolve();
+        consumer.ack({ someMessageKey: 'some-message-value' })
+          .then(() => done());
+      });
+
+      it('respects allUpTo', done => {
+        td.when(channel.ack(td.matchers.anything(), true)).thenResolve();
+        consumer.ack({ someMessageKey: 'some-message-value' }, true)
+          .then(() => done());
+      });
+    });
+
+    describe('.nack', () => {
+      it('nacks the given message', done => {
+        const expectedMessage = { someMessageKey: 'some-message-value' };
+        whenLoose(channel.nack(expectedMessage)).thenResolve();
+        consumer.nack(expectedMessage)
+          .then(() => done());
+      });
+
+      it('defaults allUpTo to false', done => {
+        whenLoose(channel.nack(td.matchers.anything(), false)).thenResolve();
+        consumer.nack({ someMessageKey: 'some-message-value' })
+          .then(() => done());
+      });
+
+      it('respects allUpTo', done => {
+        whenLoose(channel.nack(td.matchers.anything(), true)).thenResolve();
+        consumer.nack({ someMessageKey: 'some-message-value' }, true)
+          .then(() => done());
+      });
+
+      it('defaults requeue to true', done => {
+        td.when(channel.nack(td.matchers.anything(), td.matchers.anything(), true)).thenResolve();
+        consumer.nack({ someMessageKey: 'some-message-value' })
+          .then(() => done());
+      });
+
+      it('respects requeue', done => {
+        td.when(channel.nack(td.matchers.anything(), td.matchers.anything(), false)).thenResolve();
+        consumer.nack({ someMessageKey: 'some-message-value' }, false, false)
+          .then(() => done());
+      });
+    });
+
+    describe('.consume', () => {
+      it('respects the queue the consumer was created with', done => {
+        whenLoose(channel.consume(queue)).thenResolve();
+        consumer.consume(td.func())
+          .then(() => done());
+      });
+
+      it('respects the given cb', done => {
+        const expectedCb = message => `${message}-updated`;
+        whenLoose(channel.consume(
+          td.matchers.anything(),
+          td.matchers.argThat(cb => cb('some-message') === expectedCb('some-message'))
+        )).thenResolve();
+        consumer.consume(expectedCb)
+          .then(() => done());
+      });
+
+      it('defaults to empty options', done => {
+        td.when(channel.consume(
+          td.matchers.anything(),
+          td.matchers.anything(),
+          td.matchers.argThat(options => !Object.keys(options).length)
+        )).thenResolve();
+        consumer.consume(td.func())
+          .then(() => done());
+      });
+
+      it('respects the given options', done => {
+        const expectedOptions = { someKey: 'some-value' };
+        td.when(channel.consume(
+          td.matchers.anything(),
+          td.matchers.anything(),
+          expectedOptions
+        )).thenResolve();
+        consumer.consume(td.func(), expectedOptions)
+          .then(() => done());
+      });
+    });
+  });
+});
+
