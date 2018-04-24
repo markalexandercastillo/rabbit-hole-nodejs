5858915940140805c4e496baa7cb4496533237e2
Mark Castillo <markalexandercastillo@gmail.com>
2018-04-24 19:47:03 -0400

    Reformatting and adding comments in anticipation of extracting and refactoring of middleware reducing

--- src/consumer/middlewareConsumer.js
+++ src/consumer/middlewareConsumer.js
@@ -15,27 +15,34 @@ const createWithMiddlewares =
         consume: (cb, options = {}) =>
           baseConsumer.consume(
             ({message}) =>
+              // go through middlewares
               promiseReduce(
                 middlewares,
                 (args, { onMessage, onError = null }) =>
+                  // check if an error had occurred in a previous iteration
                   !args.error
+                    // call the middleware onMessage on the message argument
                     ? Promise.resolve(onMessage(args.message))
-                      .then(() => (args))
+                      // merge transformed message from onMessage consume callback args
+                      .then(message => ({...args, message}))
                       .catch(error =>
+                        // check if the middleware has an onError
                         onError
-                          ? Promise.resolve(onError({
-                            ...args,
-                            error,
-                          })).then(() => ({
-                            ...args,
-                            error,
-                          }))
+                          // invoke onError with consume callback args (for consistency)
+                          ? Promise.resolve(onError({...args, error}))
+                            // merge error into consume callback args so that the error check above does its job
+                            .then(() => ({...args, error}))
+                          // reject when there no onError is set
                           : Promise.reject(error)
                       )
+                    // do nothing, just keep passing the consume callback arguments until the iterating has completed
                     : args,
+                // initialize consume callback arguments
                 {message, error: null, ack: baseConsumer.ack, nack: baseConsumer.nack}
               ).then(args => {
+                // check if an error has been set from the middleware processing
                 if (!args.error) {
+                  // pass consume callback args into the final consume callback
                   cb(args);
                 }
               }),
