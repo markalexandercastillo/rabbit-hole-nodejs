2dae48ef2e571e1a391df0fd009e17a7b78110dd
Mark Castillo <markalexandercastillo@gmail.com>
2018-04-25 11:38:09 -0400

    Broke up middleware reducing

--- src/consumer/middlewareConsumer.js
+++ src/consumer/middlewareConsumer.js
@@ -14,39 +14,9 @@ const createWithMiddlewares =
          */
         consume: (cb, options = {}) =>
           baseConsumer.consume(
-            ({message}) =>
-              // go through middlewares
-              promiseReduce(
-                middlewares,
-                (args, { onMessage, onError = null }) =>
-                  // check if an error had occurred in a previous iteration
-                  !args.error
-                    // call the middleware onMessage on the message argument
-                    ? Promise.resolve(onMessage(args.message))
-                      // merge transformed message from onMessage consume callback args
-                      .then(message => ({...args, message}))
-                      // the onMessage callback rejected
-                      .catch(error =>
-                        // check if the middleware has an onError
-                        onError
-                          // invoke onError with consume callback args (for consistency)
-                          ? Promise.resolve(onError({...args, error}))
-                            // merge error into consume callback args so that the error check above does its job
-                            .then(() => ({...args, error}))
-                          // reject when there no onError is set
-                          : Promise.reject(error)
-                      )
-                    // do nothing, just keep passing the consume callback arguments until the iterating has completed
-                    : args,
-                // initialize consume callback arguments
-                {message, error: null, ack: baseConsumer.ack, nack: baseConsumer.nack}
-              ).then(args => {
-                // check if an error has been set from the middleware processing
-                if (!args.error) {
-                  // pass consume callback args into the final consume callback
-                  cb(args);
-                }
-              }),
+            ({ message }) =>
+              reduceMiddlewares(middlewares, baseConsumer, message)
+                .then(args => args.error ? null : cb(args)),
             options
           ),
         /**
@@ -64,11 +34,48 @@ const createWithMiddlewares =
           ),
       }));
 
+const buildInitialArgs =
+  (consumer, message) =>
+    ({
+      message,
+      error: null,
+      ack: consumer.ack,
+      nack: consumer.nack,
+    });
+
+const reduceMiddlewares =
+  (middlewares, consumer, message) =>
+    promiseReduce(
+      middlewares,
+      (args, middleware) => resolveMiddleware(middleware, args),
+      buildInitialArgs(consumer, message)
+    );
+
 const promiseReduce =
   (items, fn, init) => items.reduce(
-    (prev, curr) => prev.then(prev => fn(prev, curr)),
+    (prev, curr) => prev.then((prev => fn(prev, curr))),
     Promise.resolve(init)
   );
+
+const resolveMiddleware =
+  (middleware, args) =>
+    args.error
+      ? Promise.resolve(args)
+      : resolveOnMessage(middleware, args)
+        .then(args => args.error ? resolveOnError(middleware, args) : args);
+
+const resolveOnMessage =
+  (middleware, args) =>
+    Promise.resolve(middleware.onMessage(args.message))
+      .then(message => ({ ...args, message }))
+      .catch(error => ({ ...args, error }));
+
+const resolveOnError =
+  (middleware, args) =>
+    middleware.onError
+      ? Promise.resolve(middleware.onError(args))
+        .then(() => args)
+      : Promise.reject(args.error);
 
 module.exports = {
   create,
